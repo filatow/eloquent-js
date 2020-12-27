e8b573aa695a886b1194c58398701ba819e953ae
evgeniy <not-found@inbox.ru>
2020-12-27 19:22:15 +0300

    Исправлена ошибка в прошлом задании. Выполнено упражнение 'Эффективность робота'

--- 07/1.js
+++ 07/1.js
@@ -1,11 +1,34 @@
+function cloneDeep(target) {
+  if (typeof(target) !== 'object') {
+    return target;
+  }
+  let result;
+  if (Array.isArray(target)) {
+    result = [];
+    for (let el of target) {
+      result.push(cloneDeep(el));
+    }
+  } else {
+    result = {};
+    for (let [key, value] of Object.entries(target)) {
+      result[key] = cloneDeep(value);
+    }
+  }
+  return result;
+}
+
 function compareRobots(...robotPacks) {
-  const testCount = 10000;
-  const parcelsCount = 10;
+  const testCount = 1000;
+  const parcelsCount = 20;
   const results = [];
   for (let i = 0; i < testCount; i++) {
     const state = VillageState.random(parcelsCount);
     for (let [robot, memory] of robotPacks) {
-      let currentState = state;
+      let currentState = new VillageState(
+        state.place,
+        cloneDeep(state.parcels)
+      );
+      Object.is(currentState.parcels, state.parcels);
       for (let turn = 0; ; turn++) {
         if (currentState.parcels.length === 0) {
           results.push(turn);
@@ -18,16 +41,16 @@ function compareRobots(...robotPacks) {
     }
   }
   const firstMedium = results
-    .filter((el) => (el % 2 === 0))
-    .reduce((accum, value) => accum + value) / testCount;
+    .filter((el, ind) => (ind % 2 === 0))
+    .reduce((accum, value) => (accum + value)) / testCount;
   const secondMedium = results
-    .filter((el) => (el % 2 !== 0))
-    .reduce((accum, value) => accum + value) / testCount;
+    .filter((el, ind) => (ind % 2 !== 0))
+    .reduce((accum, value) => (accum + value)) / testCount;
   console.log("compareRobots :: firstMedium", firstMedium)
   console.log("compareRobots :: secondMedium", secondMedium)
 }
 
-compareRobots(
-  [routeRobot, mailRoute],
-  [goalOrientedRobot, []]
-);
+// compareRobots(
+//   [routeRobot, mailRoute],
+//   [goalOrientedRobot, []]
+// );
---
+++ 07/2.js
@@ -0,0 +1,47 @@
+function improvedRobot({place, parcels}, route) {
+  if (route.length == 0) {
+  const allRouts = [];
+    for (let parcel of parcels) {
+      let r;
+      if (parcel.place != place) {
+        r = findRoute(roadGraph, place, parcel.place);
+        allRouts.push({
+          route: r,
+          isDelivering: false
+        });
+      } else {
+        r = findRoute(roadGraph, place, parcel.address);
+        allRouts.push({
+          route: r,
+          isDelivering: true
+        })
+      }
+    }
+    allRouts
+      // .sort((a, b) => (a.route.length - b.route.length)); // решение 1
+      .sort((a, b) => (b.route.length - a.route.length)) // решение 2
+      .sort((a, b) => {
+        if (
+          (a.route.length === b.route.length) &&
+          (a.isDelivering === true) &&
+          (b.isDelivering === false)
+        ) {
+          return 1;
+        }
+        return -1;
+      });
+    return {
+      direction: allRouts[0].route[0],
+      memory: allRouts[0].route.slice(1)
+    }
+  }
+  return {
+    direction: route[0],
+    memory: route.slice(1)
+  }
+}
+
+compareRobots(
+  [goalOrientedRobot, []],
+  [improvedRobot, []],
+);
